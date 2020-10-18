88f8a9dfc26fc5af1ac5bb9d750a50a0ad912d36
Giorgio Boa <35845425+gioboa@users.noreply.github.com>
2020-10-18 17:13:04 +0200

    fix: funzione moltiplicazione
    Modificata la funzione moltiplicazione
    
    Fix: #1

--- moltiplicazione.js
+++ moltiplicazione.js
@@ -1,9 +1,4 @@
-const moltiplicazione = (moltiplicando, moltiplicatore) => {
-  let prodotto = 0;
-  for (let i = 0; i < moltiplicatore; i++) {
-    prodotto += moltiplicando;
-  }
-  return prodotto;
-};
+const moltiplicazione = (moltiplicando, moltiplicatore) => moltiplicando * moltiplicatore;
 
 console.log('prodotto', moltiplicazione(3, 4));
+
